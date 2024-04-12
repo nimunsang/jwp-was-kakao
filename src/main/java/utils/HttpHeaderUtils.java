708e34735c8d90c57cdb3ba246374c66049d10ec
Kong Heechan <sk980919@naver.com>
2024-04-12 15:45:16 +0900

    feat: 상수의 접근 제어자 변경

--- src/main/java/utils/HttpHeaderUtils.java
+++ src/main/java/utils/HttpHeaderUtils.java
@@ -5,7 +5,7 @@ import http.RequestStartLine;
 
 public class HttpHeaderUtils {
 
-    public static final String SPACE_DELIMITER = " ";
+    private static final String SPACE_DELIMITER = " ";
 
     public static RequestStartLine parse(String header) {
         HttpMethod httpMethod = parseHttpMethod(header);
