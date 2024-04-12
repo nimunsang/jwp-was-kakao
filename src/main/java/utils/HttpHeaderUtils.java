a1e55f575e615393382dfee48ed82b4816a52d95
Kong Heechan <sk980919@naver.com>
2024-04-12 15:44:26 +0900

    feat: 헤더 split 문자열 상수화

--- src/main/java/utils/HttpHeaderUtils.java
+++ src/main/java/utils/HttpHeaderUtils.java
@@ -4,6 +4,9 @@ import http.HttpMethod;
 import http.RequestStartLine;
 
 public class HttpHeaderUtils {
+
+    public static final String SPACE_DELIMITER = " ";
+
     public static RequestStartLine parse(String header) {
         HttpMethod httpMethod = parseHttpMethod(header);
         String url = parseUrl(header);
@@ -12,12 +15,12 @@ public class HttpHeaderUtils {
     }
 
     public static HttpMethod parseHttpMethod(String header) {
-        String[] s = header.split(" ");
+        String[] s = header.split(SPACE_DELIMITER);
         return HttpMethod.of(s[0]);
     }
 
     public static String parseUrl(String header) {
-        String[] s = header.split(" ");
+        String[] s = header.split(SPACE_DELIMITER);
         return s[1];
     }
 }
