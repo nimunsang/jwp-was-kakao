9f0f9b8c311bfdd5a13e3291c8f377860f700f2a
Kong Heechan <sk980919@naver.com>
2024-04-15 15:42:13 +0900

    refactor: key, value값 상수화

--- src/main/java/utils/QueryStringParser.java
+++ src/main/java/utils/QueryStringParser.java
@@ -6,10 +6,13 @@ import java.util.stream.Collectors;
 
 public class QueryStringParser {
 
+    private static final int KEY_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
+
     public static Map<String, String> parseToMap(String queryString) {
         String[] data = queryString.split("&");
         return Arrays.stream(data)
                 .map(it -> it.split("="))
-                .collect(Collectors.toMap(strings -> strings[0], strings -> strings[1]));
+                .collect(Collectors.toMap(strings -> strings[KEY_INDEX], strings -> strings[VALUE_INDEX]));
     }
 }
