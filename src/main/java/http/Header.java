dceeca9c43b6ce6c0e666238f3ac2d780a2851b0
Kong Heechan <sk980919@naver.com>
2024-04-15 15:41:38 +0900

    feat: 헤더 setter를 각각 정의

--- src/main/java/http/Header.java
+++ src/main/java/http/Header.java
@@ -5,6 +5,10 @@ import java.util.Map;
 import java.util.Set;
 
 public class Header {
+    public static final String LOCATION = "Location";
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String CONTENT_LENGTH = "Content-Length";
+
     private final Map<String, String> map = new HashMap<>();
 
     public void put(String key, String value) {
@@ -16,10 +20,22 @@ public class Header {
     }
 
     public int getContentLength() {
-        return Integer.parseInt(map.get("Content-Length"));
+        return Integer.parseInt(map.get(CONTENT_LENGTH));
     }
 
     public Set<String> getKeys() {
         return map.keySet();
+    }
+
+    public void setLocation(String location) {
+        map.put(LOCATION, location);
+    }
+
+    public void setContentType(ContentType contentType) {
+        map.put(CONTENT_TYPE, contentType.getValue());
+    }
+
+    public void setContentLength(int length) {
+        map.put(CONTENT_LENGTH, String.valueOf(length));
     }
 }
