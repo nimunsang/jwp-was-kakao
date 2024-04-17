d19d8f1f99960b16017534da8d10f91349066a04
Kong Heechan <sk980919@naver.com>
2024-04-17 18:13:15 +0900

    feat: redirection의 파라미터 변경 및 응답값 변경

--- src/main/java/http/redirect/RedirectionHandler.java
+++ src/main/java/http/redirect/RedirectionHandler.java
@@ -2,15 +2,23 @@ package http.redirect;
 
 import http.HttpRequest;
 import http.HttpResponse;
+import http.HttpStatus;
+import http.HttpVersion;
+import utils.HttpResponseBuilder;
 
 public class RedirectionHandler {
 
-    public static HttpResponse handle(HttpRequest httpRequest) {
+    public static HttpResponse handle(HttpRequest httpRequest, HttpResponse httpResponse) {
         String endPoint = httpRequest.getEndPoint();
         if (RedirectionMap.has(endPoint)) {
             String to = RedirectionMap.get(endPoint);
-            return Redirector.makeHttpResponse(to);
+            httpResponse.getHeader().setLocation(to);
+            return HttpResponseBuilder.builder()
+                    .httpVersion(HttpVersion.HTTP_1_1)
+                    .httpStatus(HttpStatus.FOUND)
+                    .responseHeader(httpResponse.getHeader())
+                    .build();
         }
-        return HttpResponse.notFound();
+        return httpResponse;
     }
 }
